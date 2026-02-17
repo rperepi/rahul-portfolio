f935e68b17fc8d061b563ad2556a5281126aa5cd
rperepi <163616392+rperepi@users.noreply.github.com>
2026-02-17 08:46:48 -0700

    fix next config

--- next.config.js
+++ next.config.js
@@ -1,2 +1,10 @@
-const withNextra = require('nextra')('nextra-theme-blog', './theme.config.js')
-module.exports = withNextra()
+import nextra from 'nextra'
+
+const withNextra = nextra({
+  theme: 'nextra-theme-blog',
+  themeConfig: './theme.config.js'
+})
+
+export default withNextra({
+  reactStrictMode: true
+})
